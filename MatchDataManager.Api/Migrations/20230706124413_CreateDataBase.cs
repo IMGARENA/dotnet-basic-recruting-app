3c838f04bb68e9f82dc2217ab54d1216a4909790
Czes377813 <Czes377813@PL-ZRN-WL-U5178>
2023-07-06 15:01:37 +0200

    Adding NuGet packages and creating a database

---
+++ MatchDataManager.Api/Migrations/20230706124413_CreateDataBase.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchDataManager.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    City = table.Column<string>(type: "character varying(55)", maxLength: 55, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Team",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    CoachName = table.Column<string>(type: "character varying(55)", maxLength: 55, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Team", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Location_Name",
+                table: "Location",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Team_Name",
+                table: "Team",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Location");
+
+            migrationBuilder.DropTable(
+                name: "Team");
+        }
+    }
+}
---
+++ MatchDataManager.Api/Models/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace MatchDataManager.Api.Models
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<Location> Location { get; set; }
+        public DbSet<Team> Team { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //table requirements
+            modelBuilder.Entity<Location>(eb =>
+            {
+                eb.Property(l => l.Name).IsRequired().HasMaxLength(256);
+                eb.Property(l => l.City).IsRequired().HasMaxLength(55);
+                eb.HasIndex(l => l.Name).IsUnique();
+
+            });
+
+            modelBuilder.Entity<Team>(eb =>
+            {
+                eb.Property(t => t.Name).IsRequired().HasMaxLength(256);
+                eb.Property(t => t.CoachName).IsRequired().HasMaxLength(55);
+                eb.HasIndex(t => t.Name).IsUnique();
+            });
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
